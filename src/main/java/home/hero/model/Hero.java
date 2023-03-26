0838258c0aa8d1cd1116c07c69bd6f30f1a6d385
Elmir Shamsutdinov <shamsut.elm@gmail.com>
2023-03-26 12:14:39 +0300

    updated class Hero
    added methods addStuff showInventar
    updated method move

--- src/main/java/home/hero/model/Hero.java
+++ src/main/java/home/hero/model/Hero.java
@@ -1,9 +1,12 @@
 package home.hero.model;
 
+import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
+import home.hero.WorldInitializer;
 import home.hero.area.BattleArea;
+import home.hero.stuff.AbstractStuff;
 
 public final class Hero extends AbstractPersonage {
 
@@ -13,6 +16,7 @@ public final class Hero extends AbstractPersonage {
     private static final String MOVE_DOWN = "s";
     private static final String MOVE_LEFT = "a";
     private static final String MOVE_RIGHT = "d";
+    private static final String QUETE = "q";
 
     // where is stay hero in BattleArea(start position x=9;y=0)
     private int x = 9;
@@ -33,7 +37,32 @@ public final class Hero extends AbstractPersonage {
         return instance;
     }
 
-    public static void move(Scanner scanner) {
+    public void addStuff(AbstractStuff someStuff) {
+        if (super.getInventar().length != 0) {
+            super.setInventar(new AbstractStuff[10]);
+        }
+
+        int inventaryPlace = 0;
+        for (var place : super.getInventar()) {
+            if (place != null) {
+                inventaryPlace++;
+                continue;
+            }
+            super.getInventar()[inventaryPlace] = someStuff;
+        }
+    }
+
+    // TODO make interactive
+    public void showInventar() {
+        int inventaryPlace = 1;
+        for (var stuf : super.getInventar()) {
+            System.out.println(inventaryPlace + ")" + stuf.getName() + ";");
+        }
+    }
+
+    // TODO think to do somethink with npcs and tryCounter
+    public static void move(Scanner scanner, List<NpcWithActivity> npcs,
+            int tryCounter) {
         String move = scanner.next();
         Hero hero = getInstance();
         switch (move) {
@@ -53,30 +82,42 @@ public final class Hero extends AbstractPersonage {
                 hero.setY(hero.getY() + 1);
                 break;
             }
+            case Hero.QUETE: {
+                WorldInitializer.choseWay(scanner, npcs, tryCounter);
+                break;
+            }
             default: {
-                throw new IllegalArgumentException("Unexpected value: " + scanner.next());
+                throw new IllegalArgumentException(
+                        "Unexpected value: " + scanner.next());
             }
         }
 
         int x = hero.getX();
         int y = hero.getY();
         if (x < 0) {
-            processBorderCrossing("UP", hero::setX, 0, scanner);
+            processBorderCrossing("UP", hero::setX, 0, scanner, npcs,
+                    tryCounter);
         } else if (x > 9) {
-            processBorderCrossing("DOWN", hero::setX, 9, scanner);
+            processBorderCrossing("DOWN", hero::setX, 9, scanner, npcs,
+                    tryCounter);
         } else if (y < 0) {
-            processBorderCrossing("LEFT", hero::setY, 0, scanner);
+            processBorderCrossing("LEFT", hero::setY, 0, scanner, npcs,
+                    tryCounter);
         } else if (y > 9) {
-            processBorderCrossing("RIGHT", hero::setY, 9, scanner);
+            processBorderCrossing("RIGHT", hero::setY, 9, scanner, npcs,
+                    tryCounter);
         }
     }
 
-    private static void processBorderCrossing(String side, Consumer<Integer> setter, int number, Scanner scanner) {
-        System.out.println("you can't move %s, please move another side.".formatted(side));
+    private static void processBorderCrossing(String side,
+            Consumer<Integer> setter, int number, Scanner scanner,
+            List<NpcWithActivity> npcs, int tryCount) {
+        System.out.println(
+                "you can't move %s, please move another side.".formatted(side));
         setter.accept(number);
         BattleArea.showMap();
         System.out.println("press 'w', 'a', 's', 'd' to move your hero:");
-        Hero.move(scanner);
+        Hero.move(scanner, npcs, tryCount);
     }
 
     public int getX() {
